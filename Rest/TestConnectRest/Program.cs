712dbad363477f2a1ea7686a62866805efc44fa2
yuan.jy <Jason_Yuan@epam.com>
2022-01-21 15:34:45 +0800

    Update Demo

---
+++ Rest/TestConnectRest/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Http.Headers;
+
+var url = "https://localhost:7004";
+
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri(url);
+
+var val = "application/json";
+var media = new MediaTypeWithQualityHeaderValue(val);
+
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(media);
+
+// call - get
+var action = "WeatherForecast/Test";
+var result = await client.GetAsync(action);
+if (result != null && result.IsSuccessStatusCode)
+{
+    var weatherResult = await result.Content.ReadAsStringAsync();
+    Console.WriteLine("Rest Demo: " + weatherResult);
+}
---
+++ Rest/TestConnectSoap/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ServiceModel;
+
+var serviceUrl = "https://localhost:44389/WebService.asmx";
+
+var endpointAddress = new EndpointAddress(serviceUrl);
+
+var basicHttpBinding = new BasicHttpBinding(endpointAddress.Uri.Scheme.ToLower() == "http" ?
+                BasicHttpSecurityMode.None : BasicHttpSecurityMode.Transport);
+basicHttpBinding.OpenTimeout = TimeSpan.MaxValue;
+basicHttpBinding.CloseTimeout = TimeSpan.MaxValue;
+basicHttpBinding.ReceiveTimeout = TimeSpan.MaxValue;
+basicHttpBinding.SendTimeout = TimeSpan.MaxValue;
+var SoapWebService = new SoapWebService.WebServiceSoapClient(basicHttpBinding, endpointAddress);
+var result = await SoapWebService.HelloWorldAsync();
+
+Console.WriteLine("Soap Demo: " + result.Body.HelloWorldResult);
